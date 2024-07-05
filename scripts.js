3f3ce235ede9575b5cb3692c98d0d92e70b14b5d
Tanishk-hubb <tanishknagda02@gmail.com>
2024-07-06 01:58:41 +0530

    how is the wheather

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const apiKey = fd9e81962fb66c19108bec644e625234; 
+const apiKey =//Api id (confidential); 
 
 function fetchWeather() {
     const location = document.getElementById('location-input').value;
